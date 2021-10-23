a15f7745ea34b7d690a13867605988712e214c45
edward.pinzon@gtd.com.co <EMPUMATE123>
2021-10-22 19:35:44 -0500

    entidades y html terminado

--- Aplicacion/src/main/java/com/edwarPinzon/AplicacionApplication.java
+++ Aplicacion/src/main/java/com/edwarPinzon/AplicacionApplication.java
@@ -2,8 +2,10 @@ package com.edwarPinzon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class AplicacionApplication {
 
 	public static void main(String[] args) {
